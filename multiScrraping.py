e1beddf561c9e8011f275985d7db1e03ee121097
sajagsilwal123 <sajag.silwal123@gmail.com>
2020-05-27 20:18:01 +0545

    scraped data into a List

--- multiScrraping.py
+++ multiScrraping.py
@@ -1,23 +1,33 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
-pages = [1,2,3,4]
-Data = []
+
+pages = [1]
+finalList = []
+innerList = []
 for page in pages:
     link = urlopen("http://www.nepalstock.com/main/todays_price/index/{}/".format(page))
     soup = BeautifulSoup(link, 'lxml')
     table = soup.find('table', class_ = "table table-condensed table-hover")
     temp = -1
-    """for row in table.find_all('tr'):        
+    for row in table.find_all('tr'):        
         temp = temp + 1
         for data in row.find_all('td'):
             check = data.get_text()
             if (check=='Chhimek Laghubitta Bikas Bank Limited' or check=='Bank of Kathmandu Ltd.' or check=='Upper Tamakoshi Hydropower Ltd'):
                 #print(row.get_text(),end="    ")      
-                print (table.find_all('tr')[temp].get_text())   """
+                #print (table.find_all('tr')[temp].get_text()) 
+                for finalData in row.find_all('td'):
+                    innerList.append(finalData.get_text())
+    finalList.append(innerList)
+print(finalList)
+                #finalList.append(innerList)
+                    
+                
+                
     
-date = table.find('label', {'class':'pull-left'}).text
-
+#date extraction
+"""date = table.find('label', {'class':'pull-left'}).text
 char = 6
 finalDate=''
 while char!=16:
@@ -26,4 +36,4 @@ while char!=16:
     else:
         finalDate = finalDate + date[char]
     char = char + 1
-print(finalDate)
+print(finalDate)"""
