2f1078ec3ab4d6c84956aff72923a2c00096921c
sajagsilwal123 <sajag.silwal123@gmail.com>
2020-05-29 23:03:14 +0545

    added header file to all the data and converted the data extracted from the table to csv file

--- multiScrraping.py
+++ multiScrraping.py
@@ -1,44 +1,79 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+import csv
 
-page = urlopen('http://www.nepalstock.com/main/todays_price')
+page = urlopen("http://www.nepalstock.com/main/todays_price")
 pageSoup = BeautifulSoup(page, 'lxml')
 pageDiv = pageSoup.find('div', class_= 'pager')
-
 totalPages = int(pageDiv.get_text()[-1])
 
 finalList = []
 innerTuple = ()
-for page in range(1,totalPages+1):
+
+#date extraction
+Table = pageSoup.find('table')
+date = Table.find('label', {'class':'pull-left'}).text
+char = 6
+finalDate=''
+while char!=16:
+    if (date[char]=='-'):
+        finalDate = finalDate + "."
+    else:
+        finalDate = finalDate + date[char]
+    char = char + 1
+
+
+#table header
+tableHeaderLink = Table.find('tr',{'class' : 'unique'})
+tableHeaderData = tableHeaderLink.find_all('td')
+tableHeaderTuple = ()
+a = 0
+for td in tableHeaderData:
+    a = a + 1
+    dataForTuple = td.get_text()
+    if(a==2):
+        tableHeaderTuple = tableHeaderTuple + ('Date',)
+    tableHeaderTuple = tableHeaderTuple + (dataForTuple,)
+
+
+for page in range(1,totalPages+2):
     link = urlopen("http://www.nepalstock.com/main/todays_price/index/{}/".format(page))
     soup = BeautifulSoup(link, 'lxml')
     table = soup.find('table', class_ = "table table-condensed table-hover")
-    temp = -1
+     
+    
     for row in table.find_all('tr'):        
-        temp = temp + 1
         for data in row.find_all('td'):
             check = data.get_text()
             if (check=='Chhimek Laghubitta Bikas Bank Limited' or check=='Bank of Kathmandu Ltd.' or check=='Upper Tamakoshi Hydropower Ltd'):
                 #print(row.get_text(),end="    ")      
                 #print (table.find_all('tr')[temp].get_text()) 
+                a=0
                 for finalData in row.find_all('td'):
+                    a = a+1
                     temp1 = finalData.get_text()
+                    if(a==2):
+                      innerTuple = innerTuple + (finalDate,)
                     innerTuple = innerTuple + (temp1,)
                 finalList.append(innerTuple)
                 innerTuple = ()
-result = finalList
-print(result[2])              
-                
-                
+tableData = finalList
+#print(result[0])
+
+#code to create a csv
+fileName = "{}.csv".format(finalDate)
+data = tableData
+header = tableHeaderTuple
+def writer(header, data, filename):
+    with open (filename, "w", newline = "") as csvfile:
+        company =  csv.writer(csvfile)
+        company.writerow(header)
+        for dt in data:
+            company.writerow(dt)
     
-#date extraction
-"""date = table.find('label', {'class':'pull-left'}).text
-char = 6
-finalDate=''
-while char!=16:
-    if (date[char]=='-'):
-        finalDate = finalDate + '/'
-    else:
-        finalDate = finalDate + date[char]
-    char = char + 1
-print(finalDate)"""
+    
+writer(header, data, fileName)
+
+
+#writer(tableHeaderTuple, tableData, fileName)
+
--- trial.py
+++ trial.py
@@ -1,5 +1,18 @@
-tup = (1,2,'3')
-t = 1
-tup = tup + (t,)
+import csv
 
-print(tup)
+filename = "1\3\2.csv"
+header = ("Rank", "Rating", "Title")
+data = [
+(1, 9.2, "The Shawshank Redemption(1994)"),
+(2, 9.2, "The Godfather(1972)"),
+(3, 9, "The Godfather: Part II(1974)"),
+(4, 8.9, "Pulp Fiction(1994)")
+]
+
+def writer(header, data, filename):
+      with open (filename, "w", newline = "") as csvfile:
+        movies = csv.writer(csvfile)
+        movies.writerow(header)
+        for x in data:
+          movies.writerow(x)
+writer(header, data, filename)
