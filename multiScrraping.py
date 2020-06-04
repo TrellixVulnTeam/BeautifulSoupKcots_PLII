9517b4b6a27a50049bcc4c724e3992f211155c0c
sajagsilwal123 <sajag.silwal123@gmail.com>
2020-06-04 17:48:49 +0545

    user can enter new data which is converted to all the data left to be updated to excel sheet

--- multiScrraping.py
+++ multiScrraping.py
@@ -8,72 +8,129 @@ pageDiv = pageSoup.find('div', class_= 'pager')
 totalPages = int(pageDiv.get_text()[-1])
 
 finalList = []
+insideList = []
+
+company = [] #'Bank of Kathmandu Ltd.', 'Chhimek Laghubitta Bikas Bank Limited'
+totalCompany = 0
+
+
 innerTuple = ()
+outerList = []
+addMore = input("Do you want to add more companies? (yes/no)").lower()
+while (addMore == "yes"):
+    newCompanyName = input("Enter the name of the company")
+    #typeOfcompany = input("Enter the type of Company")
+    noOfShare = input("Enter the number of Stock you possess:")
+    purchasedValue = input("Enter the value of the share purchased :")
+    company.append(newCompanyName)        
+    innerTuple = ()
+    innerTuple = innerTuple + (newCompanyName,noOfShare,purchasedValue,)
+    outerList.append(innerTuple)
+    addMore = input("Do you want to add more companies? (yes/no)").lower()
 
-#date extraction
-Table = pageSoup.find('table')
-date = Table.find('label', {'class':'pull-left'}).text
-char = 6
-finalDate=''
-while char!=16:
-    if (date[char]=='-'):
-        finalDate = finalDate + "."
-    else:
-        finalDate = finalDate + date[char]
-    char = char + 1
+for data in company:
+    totalCompany = totalCompany + 1
 
 
-#table header
-tableHeaderLink = Table.find('tr',{'class' : 'unique'})
-tableHeaderData = tableHeaderLink.find_all('td')
-tableHeaderTuple = ()
-a = 0
-for td in tableHeaderData:
-    a = a + 1
-    dataForTuple = td.get_text()
-    if(a==2):
-        tableHeaderTuple = tableHeaderTuple + ('Date',)
-    tableHeaderTuple = tableHeaderTuple + (dataForTuple,)
+
+Table = pageSoup.find('table')   
+def  getDate() :
+    date = Table.find('label', {'class':'pull-left'}).text
+    char = 6
+    finalDate=''
+    while char!=16:
+        if (date[char]=='-'):
+            finalDate = finalDate + "."
+        else:
+            finalDate = finalDate + date[char]
+        char = char + 1
+    return finalDate
+Date = getDate()
+
+def getCsvHeader():
+    tableHeaderLink = Table.find('tr',{'class' : 'unique'})
+    tableHeaderData = tableHeaderLink.find_all('td')
+    tableHeaderTuple = ()
+    a = 0
+    for td in tableHeaderData:
+        a = a + 1
+        dataForTuple = td.get_text()
+        if(a==2):
+            tableHeaderTuple = tableHeaderTuple + ('Date',)
+        tableHeaderTuple = tableHeaderTuple + (dataForTuple,)
+    return tableHeaderTuple
 
 
 for page in range(1,totalPages+2):
     link = urlopen("http://www.nepalstock.com/main/todays_price/index/{}/".format(page))
     soup = BeautifulSoup(link, 'lxml')
     table = soup.find('table', class_ = "table table-condensed table-hover")
-     
-    
     for row in table.find_all('tr'):        
         for data in row.find_all('td'):
             check = data.get_text()
-            if (check=='Chhimek Laghubitta Bikas Bank Limited' or check=='Bank of Kathmandu Ltd.' or check=='Upper Tamakoshi Hydropower Ltd'):
-                #print(row.get_text(),end="    ")      
-                #print (table.find_all('tr')[temp].get_text()) 
-                a=0
-                for finalData in row.find_all('td'):
-                    a = a+1
-                    temp1 = finalData.get_text()
-                    if(a==2):
-                      innerTuple = innerTuple + (finalDate,)
-                    innerTuple = innerTuple + (temp1,)
-                finalList.append(innerTuple)
-                innerTuple = ()
-tableData = finalList
-#print(result[0])
+            for noOfCompanies in company:
+                    if(check==noOfCompanies):              
+                        #print(row.get_text(),end="    ")      
+                        #print (table.find_all('tr')[temp].get_text()) 
+                        a=0
+                        for finalData in row.find_all('td'):
+                            a = a+1
+                            temp1 = finalData.get_text()
+                            if(a==2):
+                                 insideList.append(Date)
+                            insideList.append(temp1)
+                        finalList.append(insideList)
+                        insideList = []
+#print(finalList)
+
+                        
+FinalList = []
+finalTuple = ()            
+print("total no of company ", totalCompany)
+for i in range(totalCompany):
+    data = finalList[i]
+    tempp = int(float(data[6]))
+    for j in outerList:
+            if (j[0]==data[2]):
+                finalTuple = ()                
+                #typeOfcompany = j[3]
+                currentDate = str(data[1])
+                nameOfCompany = str(data[2])
+                noOfShare = int(j[1])
+                purchasedValuePerShare = int(j[2])
+                totalPurchasedAmount = purchasedValuePerShare * noOfShare
+                currentValuePerShare = tempp
+                totalCurrentAmount = currentValuePerShare * noOfShare
+                profitPerShare = int(currentValuePerShare) - purchasedValuePerShare
+                totalProfit = profitPerShare*noOfShare
+                profitPercent = (profitPerShare/purchasedValuePerShare)*100
+                finalTuple = finalTuple + (nameOfCompany,currentDate,noOfShare,currentValuePerShare,totalCurrentAmount,purchasedValuePerShare,totalProfit,profitPercent,)
+                break
+            finalList.append(finalTuple)
+    print(finalTuple)
+    
+#print(outerList)
+"""
+
+for d in outerListForExcel:
+    
+
+print(finalTuple)
+
+"""
 
 #code to create a csv
-fileName = "{}.csv".format(finalDate)
+"""
+fileName = "{}.csv".format(Date)
 data = tableData
-header = tableHeaderTuple
+header = getCsvHeader()
 def writer(header, data, filename):
     with open (filename, "w", newline = "") as csvfile:
         company =  csv.writer(csvfile)
         company.writerow(header)
         for dt in data:
             company.writerow(dt)
-    
-    
-writer(header, data, fileName)
-
-
 #writer(tableHeaderTuple, tableData, fileName)
+"""
+
 
--- trial.py
+++ trial.py
@@ -1,18 +1,5 @@
-import csv
+addMore = input("Do you want to add more companies? (yes/no)").lower()
 
-filename = "1\3\2.csv"
-header = ("Rank", "Rating", "Title")
-data = [
-(1, 9.2, "The Shawshank Redemption(1994)"),
-(2, 9.2, "The Godfather(1972)"),
-(3, 9, "The Godfather: Part II(1974)"),
-(4, 8.9, "Pulp Fiction(1994)")
-]
-
-def writer(header, data, filename):
-      with open (filename, "w", newline = "") as csvfile:
-        movies = csv.writer(csvfile)
-        movies.writerow(header)
-        for x in data:
-          movies.writerow(x)
-writer(header, data, filename)
+while (addMore == "yes"):
+  print(addMore)
+  break
